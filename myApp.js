6ea3a360f71119fcf711c976199ad72107e5f3b1
Damilare Elijah ISMAILA <bestbynature2010@gmail.com>
2023-11-29 18:43:46 +0000

    feat: Update express import and add route for index.html
    Update express import and add route for serving index.html

--- myApp.js
+++ myApp.js
@@ -1,39 +1,12 @@
-let express = require('express');
+let express = require("express");
 let app = express();
 
+// app.get("/", (req, res) => res.send("Hello Express"));
+app.get("/", (req, res) => {
+  const absolutePath = __dirname + "/views/index.html";
+  res.sendFile(absolutePath);
+});
 
+console.log("Hello World");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
