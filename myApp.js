3718a3d6a32b0ea6cd73f28ca7aa43074e14a484
Damilare Elijah ISMAILA <bestbynature2010@gmail.com>
2023-12-01 05:23:20 +0000

    fix: Fixed middleware logger and message value in myApp.js
    Fixed middleware logger for all routes and http methods and corrected message value

--- myApp.js
+++ myApp.js
@@ -9,11 +9,33 @@ app.get("/", (req, res) => {
 
 const absolutePathAsset = __dirname + "/public";
 
+// middleware logger or every route and for all http methods
+app.use("/", function (req, res, next) {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+});
+
+app.get(
+  "/now",
+  function (req, res, next) {
+    req.time = new Date().toString();
+    next();
+  },
+  function (req, res) {
+    res.json({
+      req: {
+        time: req.time,
+      },
+    });
+  },
+);
+
+// using middleware
 app.use("/public", express.static(absolutePathAsset));
 
 app.get("/json", (req, res) => {
   const value = process.env.MESSAGE_STYLE;
-  let message = value === "uppercase" ? "HELLO JSON" : "Hello gibson";
+  let message = value === "uppercase" ? "HELLO JSON" : "Hello json";
   res.json({ message });
 });
 
